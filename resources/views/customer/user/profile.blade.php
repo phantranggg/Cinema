84b4aae98976c87572fabc298fc89fcabf7cab44
linhlt311 <linhlt@haposoft.com>
2018-12-16 11:41:11 +0700

    Fix modal error

--- resources/views/customer/user/profile.blade.php
+++ resources/views/customer/user/profile.blade.php
@@ -136,7 +136,6 @@
                             <!-- Modal content-->
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
                                     <h4 class="modal-title">HUỶ VÉ</h4>
                                 </div>
                                 <div class="modal-body">
